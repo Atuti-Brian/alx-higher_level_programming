721020d401f9aa078f6486e90f6fde9d37354735
Atuti-Brian <atutibrian729@gmail.com>
2024-01-08 03:01:26 -0800

    8updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-    if sentence:
-        sen_len = len(sentence)
-    else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
-
-
-
-
-
-
-
-
-
-
-
-
-#!/usr/bin/python3
-def multiple_returns(sentence):
     if sentence == "":
         return 0, None
     else:
