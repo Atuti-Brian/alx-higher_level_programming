894671cc5315e26390c8639d51ae5b4be641c46e
Atuti-Brian <atutibrian729@gmail.com>
2024-01-07 23:30:34 -0800

    new_update

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = ()
+    tuple_new = ()
     tuple_x = tuple_a + (0, 0)
     tuple_y = tuple_b + (0, 0)
 
     tuple_c = tuple_x[0] + tuple_y[0], tuple_x[1] + tuple_y[1]
-    return tuple_c
-
-
-'''
-    tuple_c = ()
-    if len(tuple_a) == 2 and len(tuple_b) == 2:
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return tuple_c
-    elif len(tuple_a) < 2 and len(tuple_b) < 2:
-        tuple_a[1] = 0
-        tuple_a[1] = 0
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return tuple_c
-'''
+    return tuple_new
