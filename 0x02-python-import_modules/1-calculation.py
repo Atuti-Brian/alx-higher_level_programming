6fdd6ed53cddf9fa5c98f2f031ef1a4f5869a16a
Atuti-Brian <atutibrian729@gmail.com>
2024-01-09 00:27:38 -0800

    1-calculation-fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    g = 10
-    h = 5
-
-    print("{} + {} = {}".format(g, h, add(g, h)))
-    print("{} - {} = {}".format(g, h, sub(g, h)))
-    print("{} * {} = {}".format(g, h, mul(g, h)))
-    print("{} / {} = {}".format(g, h, div(g, h)))
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
